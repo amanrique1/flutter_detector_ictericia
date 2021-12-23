9e36e2666c133364b80ba3c1dfb5572cf0061861
Andrés Manrique <andress0199@outlook.com>
2021-12-23 16:41:13 -0500

    Color themes added

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,17 @@ class MyApp extends StatelessWidget {
         bloc: BlocsManager(),
         child: MaterialApp(
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        primaryColor: Colors.indigo.shade200,
+        accentColor: Colors.teal,
+        brightness: Brightness.light
+
+      ),
+      darkTheme: ThemeData(
+          primarySwatch: Colors.indigo,
+          primaryColor: Colors.indigo.shade200,
+          accentColor: Colors.teal,
+          brightness: Brightness.dark
       ),
       home: LogInScreen()
     )
